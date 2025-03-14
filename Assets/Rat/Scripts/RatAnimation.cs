eb3b58fae6723934020b3b54307cf065d8ced092
Vladimir Kotsev <v.kotsev04@gmail.com>
2025-03-14 20:56:12 +0200

    Update RatAnimations

--- Assets/Rat/Scripts/RatAnimation.cs
+++ Assets/Rat/Scripts/RatAnimation.cs
@@ -4,10 +4,14 @@ public class MouseAnimation : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody2D rb;
+    private Vector2 lastPosition;
 
     void Start()
     {
-;
+        animator = GetComponentInChildren<Animator>(); // Find Animator in child
+        rb = GetComponent<Rigidbody2D>(); // Get Rigidbody if using physics
+
+        lastPosition = transform.position;
     }
 
     void Update()
@@ -15,5 +19,15 @@ public class MouseAnimation : MonoBehaviour
         // Calculate movement speed
         float speed = ((Vector2)transform.position - lastPosition).magnitude / Time.deltaTime;
         lastPosition = transform.position;
+
+        // Set animation state based on speed
+        if (speed > 0.05f) // Small threshold to avoid jitter
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
 }
