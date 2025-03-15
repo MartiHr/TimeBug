3c5bde35c2e6bcc6432d95fbe8739ca39045c008
Vladimir Kotsev <v.kotsev04@gmail.com>
2025-03-15 14:06:29 +0200

    Add GameAudioManager
    GameAudioManager for game music

---
+++ Assets/Character/Scripts/GameAudioManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GameAudioManager : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioSource SFXSource;
+
+    public AudioClip background;
+    public AudioClip portal;
+
+    public static GameAudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
